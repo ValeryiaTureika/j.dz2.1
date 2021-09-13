b9bbc5532c9528e7eab48de0e39a95e81092068e
Valeria Hurava <ler4ik65@gmail.com>
2021-09-13 15:27:05 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3600;
+        int priceOfOneBonusMile = 20;
+        int onlyMiles = ticketPrice / priceOfOneBonusMile;
+        System.out.println(onlyMiles);
+    }
+}
